fe4430621af1d23847a7917233ed1635d9432749
anaisrandria <anais.randriamitantsoa@gmail.com>
2024-11-12 17:54:59 +0100

    add: product component, dynamic routes on card click, sticky nav bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
 
     return (
     <>
-    <div className=" w-full flex items-center justify-between px-4 py-2 bg-white shadow-md">
+    <div className="sticky z-10 top-0 w-full flex items-center justify-between px-4 py-2 bg-white shadow-md">
         <a className="self-start rounded-lg px-4 py-2 hover:text-slate-700 text-slate-700 font-medium" href="/">meublio.</a>
         <input placeholder="Recherche..." className="rounded-lg px-4 py-2 max-w-sm w-full text-slate-700 border border-slate-300 focus:outline-none focus:ring-2 focus:ring-slate-500" type="text"></input>
         <div className="self-end rounded-lg px-3 py-2 text-slate-700 font-medium">
